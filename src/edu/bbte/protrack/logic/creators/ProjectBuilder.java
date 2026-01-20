b7a3adbdc5b6ed67fa9d20ad2d8b929d9baf377a
OrsFazakas <ricsi6372@gmail.com>
2026-01-21 00:41:27 +0200

    vegleges

--- src/edu/bbte/protrack/logic/creators/ProjectBuilder.java
+++ src/edu/bbte/protrack/logic/creators/ProjectBuilder.java
@@ -2,23 +2,15 @@ package edu.bbte.protrack.logic.creators;
 
 import edu.bbte.protrack.model.entities.TaskGroup;
 import edu.bbte.protrack.model.entities.ProjectComponent;
-import java.util.Date;
-
 
 //Builder minta a komplex projektek (TaskGroup) összeállításához.
 public class ProjectBuilder {
     private String projectName;
     private TaskGroup rootGroup;
-    private Date deadline;
 
     public ProjectBuilder(String name) {
         this.projectName = name;
         this.rootGroup = new TaskGroup(name);
-    }
-
-    public ProjectBuilder setDeadline(Date deadline) {
-        this.deadline = deadline;
-        return this;
     }
 
     public ProjectBuilder addInitialComponent(ProjectComponent component) {
